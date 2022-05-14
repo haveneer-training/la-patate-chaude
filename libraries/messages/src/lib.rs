41c822a14dfe09de4390842f53d5daafdd6b4646
paulb <barrie.paul@protonmail.com>
2022-05-14 19:55:31 +0200

    wip: issue on Challenge & Challenge Result using the enum of structs -> how to access value then?

--- libraries/messages/src/lib.rs
+++ libraries/messages/src/lib.rs
@@ -80,7 +80,7 @@ pub mod message {
         pub fn to_string(public_player: &PublicPlayer) -> String {
             return serde_json::to_string(public_player).unwrap();
         }
-        pub fn from_str(s: &str) -> PublicPlayer {
+        pub fn from_string(s: &str) -> PublicPlayer {
             return serde_json::from_str(s).unwrap();
         }
     }
@@ -97,14 +97,119 @@ pub mod message {
         pub fn to_string(public_leader_board: &PublicLeaderBoard) -> String {
             return serde_json::to_string(public_leader_board).unwrap();
         }
-        pub fn from_str(s: &str) -> PublicLeaderBoard {
+        pub fn from_string(s: &str) -> PublicLeaderBoard {
             return serde_json::from_str(s).unwrap();
         }
-    }   
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum ChallengeType {
+        MD5HashCash {
+            complexity: i32,
+            message: String
+        },
+    }
+    impl ChallengeType {
+        pub fn new_md5_hashcash(complexity: i32, message: String) -> ChallengeType {
+            return ChallengeType::MD5HashCash{ complexity, message };
+        }
+        pub fn to_string(challenge_type: &ChallengeType) -> String {
+            return serde_json::to_string(challenge_type).unwrap();
+        }
+        pub fn from_string(s: &str) -> ChallengeType {
+            return serde_json::from_str(s).unwrap();
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Challenge {
+        #[serde(rename(serialize = "Challenge", deserialize = "Challenge"))]
+        pub challenge: ChallengeType,
+    }
+    impl Challenge {
+        pub fn new(challenge: ChallengeType) -> Challenge {
+            return Challenge { challenge };
+        }
+        pub fn to_string(challenge: &Challenge) -> String {
+            return serde_json::to_string(challenge).unwrap();
+        }
+        pub fn from_string(s: &str) -> Challenge {
+            return serde_json::from_str(s).unwrap();
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum ChallengeAnswerType {
+        MD5HashCash {
+            seed: String,
+            hashcode: String
+        },
+    }
+    impl ChallengeAnswerType {
+        pub fn new_md5_hashcash(seed: String, hashcode: String) -> ChallengeAnswerType {
+            return ChallengeAnswerType::MD5HashCash{ seed, hashcode };
+        }
+        pub fn to_string(challenge_answer_type: &ChallengeAnswerType) -> String {
+            return serde_json::to_string(challenge_answer_type).unwrap();
+        }
+        pub fn from_string(s: &str) -> ChallengeAnswerType {
+            return serde_json::from_str(s).unwrap();
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct ChallengeAnswer {
+        pub answer: ChallengeAnswerType,
+        pub next_target: String,
+    }
+    impl ChallengeAnswer {
+        pub fn new(answer: ChallengeAnswerType, next_target: String) -> ChallengeAnswer {
+            return ChallengeAnswer { answer, next_target };
+        }
+        pub fn to_string(challenge_answer: &ChallengeAnswer) -> String {
+            return serde_json::to_string(challenge_answer).unwrap();
+        }
+        pub fn from_string(s: &str) -> ChallengeAnswer {
+            return serde_json::from_str(s).unwrap();
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct ReportedChallengeResult {
+        name: String,
+        value: String,
+    }
+    impl ReportedChallengeResult {
+        pub fn new(name: String, value: String) -> ReportedChallengeResult {
+            return ReportedChallengeResult { name, value };
+        }
+        pub fn to_string(repported_challenge_result: &ReportedChallengeResult) -> String {
+            return serde_json::to_string(repported_challenge_result).unwrap();
+        }
+        pub fn from_string(s: &str) -> ReportedChallengeResult {
+            return serde_json::from_str(s).unwrap();
+        }
+    }
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct RoundSummary {
+        challenge: String,
+        chain: Vec<ReportedChallengeResult>
+    }
+    impl RoundSummary {
+        pub fn new(challenge: String, chain: Vec<ReportedChallengeResult>) -> RoundSummary {
+            return RoundSummary { challenge, chain };
+        }
+        pub fn to_string(round_summary: &RoundSummary) -> String {
+            return serde_json::to_string(round_summary).unwrap();
+        }
+        pub fn from_string(s: &str) -> RoundSummary {
+            return serde_json::from_str(s).unwrap();
+        }
+    }
 
 }
 
-#[cfg(test)]
+#[cfg(test)]    
 mod tests {
     // Hello tests
     #[test]
@@ -154,7 +259,7 @@ mod tests {
     }
     #[test]
     fn public_player_deserialize_test() {
-        let public_player = super::message::PublicPlayer::from_str("{\"name\":\"name\",\"stream_id\":\"stream_id\",\"score\":1,\"steps\":1,\"is_active\":true,\"total_used_time\":1.0}");
+        let public_player = super::message::PublicPlayer::from_string("{\"name\":\"name\",\"stream_id\":\"stream_id\",\"score\":1,\"steps\":1,\"is_active\":true,\"total_used_time\":1.0}");
         assert_eq!(public_player.name, "name");
     }
     #[test]
@@ -164,7 +269,7 @@ mod tests {
     }
     #[test]
     fn public_leaderboard_deserialize_test() {
-        let public_leader_board = super::message::PublicLeaderBoard::from_str("[{\"name\":\"name\",\"stream_id\":\"stream_id\",\"score\":1,\"steps\":1,\"is_active\":true,\"total_used_time\":1.0}]");
+        let public_leader_board = super::message::PublicLeaderBoard::from_string("[{\"name\":\"name\",\"stream_id\":\"stream_id\",\"score\":1,\"steps\":1,\"is_active\":true,\"total_used_time\":1.0}]");
         assert_eq!(public_leader_board.players[0].name, "name");
         assert_eq!(public_leader_board.players[0].stream_id, "stream_id");
         assert_eq!(public_leader_board.players[0].score, 1);
@@ -173,4 +278,38 @@ mod tests {
         assert_eq!(public_leader_board.players[0].total_used_time, 1.0);
     }
 
+    // Challenge tests
+    #[test]
+    fn challenge_serialize_test() {
+        let challenge = super::message::Challenge::new(super::message::ChallengeType::new_md5_hashcash(1, "message".to_string()));
+        assert_eq!(super::message::Challenge::to_string(&challenge), "{\"Challenge\":{\"MD5HashCash\":{\"complexity\":1,\"message\":\"message\"}}}");
+    }
+    // #[test]
+    // fn challenge_deserialize_test() {
+    //     let challenge = super::message::Challenge::from_string("{\"Challenge\":{\"MD5HashCash\":{\"complexity\":1,\"message\":\"message\"}}}");
+    //     assert_eq!(challenge.challenge.MD5HashCash, super::message::ChallengeType::new_md5_hashcash(1, "message".to_string()));
+    // }
+
+    // ChallengeResult tests
+    // TODO
+
+    // RoundSummary tests
+    // #[test]
+    // fn round_summary_serialize_test() {
+    //     let challenge = super::message::Challenge::new(super::message::ChallengeType::new_md5_hashcash(1, "message".to_string()));
+    //     let round_summary = super::message::RoundSummary::new(challenge, vec![super::message::PublicPlayer::new("name".to_string(), "stream_id".to_string(), 1, 1, true, 1.0)]);
+    //     assert_eq!(super::message::RoundSummary::to_string(&round_summary), "{\"Challenge\":{\"MD5HashCash\":{\"complexity\":1,\"message\":\"message\"}},\"LeaderBoard\":[{\"name\":\"name\",\"stream_id\":\"stream_id\",\"score\":1,\"steps\":1,\"is_active\":true,\"total_used_time\":1.0}]}");
+    // }
+    // #[test]
+    // fn round_summary_deserialize_test() {
+    //     let challenge = super::message::Challenge::new(super::message::ChallengeType::new_md5_hashcash(1, "message".to_string()));
+    //     let round_summary = super::message::RoundSummary::from_string("{\"Challenge\":{\"MD5HashCash\":{\"complexity\":1,\"message\":\"message\"}},\"LeaderBoard\":[{\"name\":\"name\",\"stream_id\":\"stream_id\",\"score\":1,\"steps\":1,\"is_active\":true,\"total_used_time\":1.0}]}");
+    //     assert_eq!(round_summary.challenge.MD5HashCash, super::message::ChallengeType::new_md5_hashcash(1, "message".to_string()));
+    //     assert_eq!(round_summary.leader_board[0].name, "name");
+    //     assert_eq!(round_summary.leader_board[0].stream_id, "stream_id");
+    //     assert_eq!(round_summary.leader_board[0].score, 1);
+    //     assert_eq!(round_summary.leader_board[0].steps, 1);
+    //     assert_eq!(round_summary.leader_board[0].is_active, true);
+    //     assert_eq!(round_summary.leader_board[0].total_used_time, 1.0);
+    // }
 }
