0460db89c79865935ea8ad38468ed0f483b4c035
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2022-11-29 16:29:10 +0100

    add the command markdown file for the command to run in the project and add the base of the project to speak with the server

---
+++ COMMAND.md
@@ -0,0 +1,15 @@
+### Command to execute the server
+##### Go in the good directory who is "distribution/macos" for MacOS and "distribution/windows" for Windows and run :
+```bash
+./server
+```
+---
+### Command to execute the server in debug mode 😖
+```bash
+./server --debug
+```
+---
+### Command to execute the client 😄
+```bash
+cargo run --bin client
+```
---
+++ client/app.rs
@@ -0,0 +1,12 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+    let message = "I'm trying to speak with you 😄";
+
+    println!("{}", message.len());
+    let len = message.len() as u32;
+    stream.write(&len.to_be_bytes()).unwrap(); // on écrit le préfixe (taille du prochain message)
+    stream.write(message.as_bytes()).unwrap(); // puis le message en tant que tel
+}
