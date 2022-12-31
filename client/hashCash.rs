cf166473dc7c4a94c28f9b1ffb2041a78e456785
sarah-bouadi <sarahbouadi0508@gmail.com>
2022-12-31 10:35:56 +0100

    Adding hashcash input and output structures and message to hash

--- client/hashCash.rs
+++ client/hashCash.rs
@@ -1,1 +1,21 @@
-/*md5-hashcash-file*/
+// Données en entrée du challenge
+struct MD5HashCashInput {
+    // complexity in bits
+    complexity: u32,
+    // message to sign
+    message: String,
+}
+
+// Données en sortie du challenge
+struct MD5HashCashOutput {
+    // Seed used to solve the challenge
+    seed: u64,
+    // hashcode found using seed + message
+    hashcode: String,
+}
+
+// Generate a correct hashcode
+/*pub fn getHashMd5(input: MD5HashCashInput) -> MD5HashCashOutput {
+
+}*/
+
